6f0faea8e91333f22915bcff39531895acafe36d
hellcy <chengyuan82281681@hotmail.com>
2019-09-01 19:38:40 +1000

    more

--- 1177. Can Make Palindrome from Substring.java
+++ 1177. Can Make Palindrome from Substring.java
@@ -5,39 +5,34 @@ class Solution {
                 we know there are only 26 characters in the English alphabet,
                 which means if k is equal to or greater than 13, we can definately make it to palindrome
                 otherwise, we keep track of the number of appearance of each character, 
-                and add 1 to count only if the appearance time is odd.
+                and add character to the set only if the appearance time is odd.
                 if distinctive character is more than 2 * k, return false
         */
         int n = queries.length;
         List<Boolean> array = new ArrayList<>();
-        
         for (int i = 0; i < n; ++i) {
-            String subString = s.substring(queries[i][0], queries[i][1] + 1);
-            if (queries[i][2] >= 13) { 
+            if (queries[i][2] >= 13) {
                 array.add(true);
                 continue;
             }
-            array.add(check(subString, queries[i][2]));
+            String substring = s.substring(queries[i][0], queries[i][1] + 1);
+            array.add(check(substring, queries[i][2]));
         }
         
         return array;
     }
     
     private boolean check(String s, int k) {
-        int count = 0;
         char[] array = s.toCharArray();
-        Map<Character, Integer> map = new HashMap<>();
+        Set<Character> set = new HashSet<>();
         for (int i = 0; i < array.length; ++i) {
-            if (map.containsKey(array[i])) {
-                map.remove(array[i]);
-                count--;
+            if (set.contains(array[i])) {
+                set.remove(array[i]);
             } else {
-                map.put(array[i], 1);
-                count++;
+                set.add(array[i]);
             }
-        } 
-
-        if (count / 2 <= k) return true;
-        return false;
+        }
+        
+        return set.size() / 2 <= k;
     }
 }

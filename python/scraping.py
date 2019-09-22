19ce866d995607a9d4b301e20524eb1d2b4991da
yuan <chengyuan82281681@hotmail.com>
2019-09-23 09:36:51 +1000

    read me

--- python/scraping.py
+++ python/scraping.py
@@ -12,13 +12,24 @@ table = soup.find('table', {"class": "table table-striped"})
 rows = table.find_all('tr')
 for r in rows:
 	data = r.find_all('td', {"label": "[object Object]"})
+	name = "";
 	for td in data:
-		if td.get_text() == None or td.get_text() == "":
-			continue
 		a = td.find('a', href=True)
-		if a != None:
-			f.write('|[' + td.get_text() + ']')
-			f.write('('+a['href']+')||')
+		if td.get_text() == "" and a == None and name != "":
+			f.write('||[Java]')
+			f.write('(./Algorithms/' + name + '.java)')
+			continue
+		elif td.get_text() == "" and a == None:
+			continue
+		elif a != None and td.get_text() != "":
+			f.write('|[' + td.get_text().strip() + ']')
+			f.write('('+a['href']+')')
+			name = td.get_text().replace(" ", "").strip()
+		elif a != None and td.get_text() == "":
+			f.write('|[Solution]')
+			f.write('('+a['href']+')')
+			f.write('|[Java]')
+			f.write('(./Algorithms/' + name + '.java)')
 		else:
 			f.write('|' + td.get_text())
 	f.write('|\n')

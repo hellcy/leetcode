0361b95038bbcfbb37499649e259d4b6e2ada848
yuan <chengyuan82281681@hotmail.com>
2019-10-22 10:35:08 +1100

    more

--- python/scraping.py
+++ python/scraping.py
@@ -1,11 +1,11 @@
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 
-path = 'C:/leetcode/python/LeetCode3.html'
+path = 'C:/leetcode/python/LeetCode.html'
 
 htmlfile = open(path, 'r', encoding='utf-8').read()
 
-f = open("README.md", "a", encoding='utf-8')
+f = open("README.md", "w", encoding='utf-8')
 
 soup = BeautifulSoup(htmlfile, features='lxml')
 table = soup.find('table', {"class": "table table-striped"})

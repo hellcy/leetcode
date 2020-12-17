fcbff5c0e931f147d2eb46e9c9a1961c594b4c29
yuan <chengyuan82281681@hotmail.com>
2020-12-18 10:20:11 +1100

    more

--- Algorithms/1254. Number of Closed Islands.java
+++ Algorithms/1254. Number of Closed Islands.java
@@ -24,39 +24,41 @@ class Solution {
                 if (grid[i][j] == 0) {
                     count++;
                 } else {
-                    int index = row * i + j;
+                    int index = col * i + j;
                     arr[index] = -1;
                 }
             }
         }
 
         for (int i = 0; i < arr.length; ++i) {
-            if (arr[i] == -1) continue;
-
+            int x = i / col;
+            int y = i % col;
+            if (find(arr, i) == -1) continue;
             Queue<int[]> queue = new LinkedList<>();
-            int x = i % row;
-            int y = i / row;
             queue.add(new int[] {x, y});
-            int[] directionx = {0, 1, 0, -1};
+            int[] directionx = {0, -1, 0, 1};
             int[] directiony = {1, 0, -1, 0};
             while (!queue.isEmpty()) {
                 int[] cur = queue.poll();
-                int index = cur[0] * row + cur[1];
-                for (int j = 0; j < 4; ++i) {
+                int index = cur[0] * col + cur[1];
+                for (int j = 0; j < 4; ++j) {
                     int newx = directionx[j] + cur[0];
                     int newy = directiony[j] + cur[1];
 
                     if (newx < 0 || newx >= row || newy < 0 || newy >= col) {
-                        arr[index] = -1;
+                        int num = find(arr, index);
+                        if (num != -1) arr[num] = -1;
                         continue;
                     }
                     if (grid[newx][newy] == 1) continue;
                     int root_cur = find(arr, index);
-                    int root_new = find(arr, newx * row + newy);
+                    int root_new = find(arr, newx * col + newy);
                     if (root_cur != root_new) {
-                        arr[root_new] = root_cur;
+                        if (root_cur == -1) arr[root_new] = -1;
+                        else if (root_new == -1) arr[root_cur] = -1;
+                        else arr[root_new] = arr[root_cur];
+                        queue.add(new int[] {newx, newy});
                     }
-                    queue.add(new int[] {newx, newy});
                 }
             }
         }
@@ -64,14 +66,16 @@ class Solution {
         Set<Integer> set = new HashSet<>();
         for (int i = 0; i < arr.length; ++i) 
         {
-            if (arr[i] == -1) continue;
-            set.add(arr[i]);
+            int num = find(arr, i);
+            if (num == -1) continue;
+            set.add(num);
         }
+
         return set.size();
     }
 
     private int find(int[] arr, int x) {
-        if (arr[x] == x) return x;
+        if (x == -1 || arr[x] == x) return x;
         return arr[x] = find(arr, arr[x]);
     }
 }
